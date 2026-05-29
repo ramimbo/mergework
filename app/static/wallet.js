b0b601ddd953ce68cd379b1791cb2e3111372dec
tinyopsstudio <support@tinyopsstudio.com>
2026-05-28 23:32:57 -0400

    Canonicalize wallet JSON unicode keys

--- app/static/wallet.js
+++ app/static/wallet.js
@@ -231,6 +231,8 @@ function setupTransfer() {
       await getNextNonce(fromAddress, "[data-transfer-nonce-status]");
     } catch (error) {
       setText("[data-transfer-result]", error.message);
+    } finally {
+      clearPrivateKeyField(form);
     }
   });
 }
